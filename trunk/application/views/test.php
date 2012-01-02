9e7a6c90c9bfaa9ec3be5944f2f0f64525253212
ottokang <ottokang@f80435aa-12b1-d9ab-ff16-a3fdf585a9ca>
2012-01-02 12:36:29 +0000

    - 初次版本

---
+++ trunk/application/views/test.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="zh">
+    <head>
+        <meta charset="utf-8">
+        <title><?php echo $title ?></title>
+    </head>
+    <body>
+        <div id="container">
+            <h1>Welcome to CodeIgniter!</h1>
+        </div>
+    </body>
+</html>
