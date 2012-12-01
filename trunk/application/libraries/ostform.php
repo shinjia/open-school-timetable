bbd6d928c6df28f47647984677c3e8dab2887187
ottokang <ottokang@f80435aa-12b1-d9ab-ff16-a3fdf585a9ca>
2012-12-01 07:35:58 +0000

    - 登入表單修改完成 - 建立資料庫

--- trunk/application/libraries/ostform.php
+++ trunk/application/libraries/ostform.php
@@ -2,16 +2,26 @@
 /**
  * OST Form 類別
  */
-class OSTForm extends Form
+class OSTForm
 {
-	public static function textLine($name, $label, $value = NULL, $attribs = array())
+	public static function text($name, $label, $value = NULL, $attribs = array())
 	{
 		return '<tr><td class="label">' . Form::label($name, $label) . '</td><td class="input_field">' . Form::text($name, $value, $attribs) . '</td></tr>';
+	}
+
+	public static function password($name, $label, $attribs = array())
+	{
+		return '<tr><td class="label">' . Form::label($name, $label) . '</td><td class="input_field">' . Form::password($name, $attribs) . '</td></tr>';
 	}
 
 	public static function open($action = NULL, $method = 'POST', $attributes = array(), $https = NULL)
 	{
 		return Form::open($action, $method, $attributes, $https) . '<table class="form_table">';
+	}
+
+	public static function submit($name)
+	{
+		return '<tr><td colspan="2" class="submit">' . Form::submit($name) . '</td></tr>';
 	}
 
 	public static function close()
--- trunk/application/views/login/index.php
+++ trunk/application/views/login/index.php
@@ -1,4 +1,8 @@
-<h1>登入畫面</h1>
+<?php Asset::add('global', 'css/form.css'); ?>
+<?php Asset::add('login', 'css/login.css'); ?>
+<h1>使用者登入</h1>
 <?= OSTForm::open(URL::to('login')) ?>
-<?= OSTForm::textLine('name', '帳號') ?>
+<?= OSTForm::text('name', '帳號', NULL, array('autofocus' => 'autofocus')) ?>
+<?= OSTForm::password('password', '密碼') ?>
+<?= OSTForm::submit('登入') ?>
 <?= OSTForm::close() ?>
