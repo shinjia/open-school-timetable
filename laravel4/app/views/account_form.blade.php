acdbb750fd20c3497342b0e064309098776bcc5a
ottokang@gmail.com <ottokang@gmail.com@f80435aa-12b1-d9ab-ff16-a3fdf585a9ca>
2013-10-01 12:21:02 +0000

    - 錯字修正

--- laravel4/app/views/account_form.blade.php
+++ laravel4/app/views/account_form.blade.php
@@ -24,12 +24,6 @@
 	{{ OstForm::description('請輸入以下的資料') }}
 	{{ OstForm::text('teacher_name', '教師姓名', array('autofocus' => 'autofocus', 'required' => 'required')) }}
 	{{ OstForm::text('teacher_account', '帳號', array('placeholder' => '英文+數字', 'required' => 'required')) }}
-
-	if ($formType == 'edit')
-		{{ OstForm::open($teacher, 'account/edit/' . $teacher->teacher_id) }}
-	@endif
-
-	{{ (isset($editTeacherForm)) ?  : '' }}
 	{{ OstForm::password('teacher_password', '密碼') }}
 	{{ OstForm::password('teacher_password_confirmation', '確認密碼') }}
 
