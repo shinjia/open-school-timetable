e76469051358aacebde0e673fb58c4ea560b95d5
ottokang@gmail.com <ottokang@gmail.com@f80435aa-12b1-d9ab-ff16-a3fdf585a9ca>
2013-12-13 18:26:08 +0000

    - 刪除舊有版本

--- old/application/models/Teacher.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Teacher extends Eloquent
-{
-	public static $table = 'teacher';
-	public static $key = 'teacher_id';
-	public static $timestamps = false;
-}
-?>
--- old/application/views/login/index.php
+++
@@ -1,9 +0,0 @@
-<?php Asset::add('global', 'css/form.css'); ?>
-
-<h1>使用者登入</h1>
-<?= OstForm::open(URL::to('login')) ?>
-<?= OstForm::description('請輸入帳號密碼') ?>
-<?= OstForm::text('name', '帳號', NULL, array('autofocus' => 'autofocus')) ?>
-<?= OstForm::password('password', '密碼') ?>
-<?= OstForm::submit('登入') ?>
-<?= OstForm::close() ?>
